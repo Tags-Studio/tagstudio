2c259c00ed95990f205f0bef5d31ef985886a561
abozahran <107565623+abozahran@users.noreply.github.com>
2026-08-26 12:31:23 +0300

    perf: defer Google Tag Manager scripts with lazyOnload to improve mobile TBT

--- app/layout.tsx
+++ app/layout.tsx
@@ -124,9 +124,9 @@ export default function RootLayout({
         {/* Google Analytics Script */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-7EET2GCC8N"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="lazyOnload">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
